1eff02186c782ff832ca72553fc4ec89b2f7c80a
Hanz Maverick <maverick_ruiz@mesoamericana.edu.gt>
2023-11-13 19:18:17 -0600

    Camios a nivel general

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,93 @@
 <template>
   <div>
-    <b-navbar class="oculto-impresion" style="border: 2px solid #ffe08a">
+    <b-navbar class="oculto-impresion" style="background: linear-gradient(to right, #e75528, #ffba97);">
       <template #brand>
         <b-navbar-item tag="a" target="_blank" :href="logo">
           <img :src="logo" style="min-height: 85px" />
         </b-navbar-item>
       </template>
       <template #start>
-        <b-navbar-item tag="router-link" :to="{ name: 'Recetario' }"
-          ><b-icon icon="chef-hat"></b-icon>&nbsp;Recetario</b-navbar-item
+        <b-navbar-item
+          tag="router-link"
+          :to="{ name: 'Recetario' }"
+          @click="setActiveSection('Recetario')"
+          :class="{ 'active-section': activeSection === 'Recetario' }"
         >
-        <b-navbar-item tag="router-link" :to="{ name: 'NuevaReceta' }"
-          ><b-icon icon="plus" class="mr-1"></b-icon>Nueva receta</b-navbar-item
+          <b-icon icon="chef-hat"></b-icon>&nbsp;Recetario
+        </b-navbar-item>
+        <b-navbar-item
+          tag="router-link"
+          :to="{ name: 'NuevaReceta' }"
+          @click="setActiveSection('NuevaReceta')"
+          :class="{ 'active-section': activeSection === 'NuevaReceta' }"
         >
-        <b-navbar-item tag="router-link" :to="{ name: 'Recetas' }"
-          ><b-icon icon="book-open-page-variant"></b-icon>&nbsp;Gestionar
-          recetas</b-navbar-item
+          <b-icon icon="plus" class="mr-1"></b-icon>Nueva receta
+        </b-navbar-item>
+        <b-navbar-item
+          tag="router-link"
+          :to="{ name: 'Recetas' }"
+          @click="setActiveSection('Recetas')"
+          :class="{ 'active-section': activeSection === 'Recetas' }"
         >
-        <b-navbar-item tag="router-link" :to="{ name: 'AcercaDe' }"
-          ><b-icon icon="information"></b-icon>&nbsp;Acerca de</b-navbar-item
+          <b-icon icon="book-open-page-variant"></b-icon>&nbsp;Gestionar recetas
+        </b-navbar-item>
+        <b-navbar-item
+          tag="router-link"
+          :to="{ name: 'AcercaDe' }"
+          @click="setActiveSection('AcercaDe')"
+          :class="{ 'active-section': activeSection === 'AcercaDe' }"
         >
+          <b-icon icon="information"></b-icon>&nbsp;Acerca de Nosotros
+        </b-navbar-item>
       </template>
       <template #end>
-        <b-navbar-item tag="div">
-          <div class="buttons">
-            <a href="https://parzibyte.me/#contacto" class="button is-danger">
-              <b-icon icon="help"></b-icon>
-              <strong>Ayuda y soporte</strong>
-            </a>
-          </div>
-        </b-navbar-item>
+        <b-navbar-item tag="div"></b-navbar-item>
       </template>
     </b-navbar>
     <section class="section sin-espacios-al-imprimir">
-      <router-view></router-view>
+      <transition name="fade" mode="out-in">
+        <router-view />
+      </transition>
+      <div v-if="showPreloader" class="preloader">
+        <img @load="onPreloaderLoad" src="./assets/img/preloader.gif" alt="Preloader" />
+      </div>
     </section>
-    <footer class="footer oculto-impresion">
+    <footer class="footer oculto-impresion" style="background-color: #f0f0f0; padding: 20px;">
       <div class="content has-text-centered">
-        <p>
-          <strong>Gestión de recetas - Software de recetario </strong>creado y
-          mantenido con <b-icon icon="heart" type="is-danger"></b-icon> por
-          <a href="https://parzibyte.me/blog">Parzibyte</a>
-        </p>
+        <div class="copyright">© Todos los derechos reservados 2023</div>
       </div>
     </footer>
   </div>
 </template>
 
+<script src="js/bootstrap.min.js"></script>
 <script>
-import Logo from "./assets/parzibyte.png";
+import Logo from "@/assets/img/Whack-Dark.png";
+
 export default {
   name: "app",
   data: () => ({
     logo: Logo,
+    activeSection: null,
+    showPreloader: false,
   }),
-  methods: {},
+  methods: {
+    setActiveSection(section) {
+      this.showPreloader = true; // Mostrar preloader al hacer clic
+    },
+    onPreloaderLoad() {
+      // Este evento se dispara cuando el gif se ha cargado completamente
+      setTimeout(() => {
+        this.activeSection = section;
+        this.showPreloader = false; // Ocultar preloader después de la transición
+      }, 1000); // Ajusta el tiempo según tus necesidades
+    },
+  },
 };
 </script>
-<style >
+
+<style scoped>
+/* Estilos específicos del componente */
 @media print {
   .oculto-impresion,
   .oculto-impresion * {
@@ -68,4 +98,16 @@ export default {
     padding: 7px !important;
   }
 }
+.preloader {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: rgba(255, 255, 255, 0.9); /* Fondo blanco semitransparente */
+}
+
 </style>
--- src/components/AcercaDe.vue
+++ src/components/AcercaDe.vue
@@ -4,35 +4,40 @@
       <section class="hero is-link">
         <div class="hero-body">
           <p class="title">
-            Software para recetario online creado con PHP y Vue
+            WHACK, Recetario online.
+            ¡Contáctanos!
+          </p>
+          <p class="title">
+            De todos y para todos.
           </p>
           <p class="subtitle">
-            Creado por
-            <a class="has-text-warning" href="https://parzibyte.me/blog"
-              >Parzibyte</a
-            >
+          ㅤ
           </p>
-          <p>
-            Si necesitas ayuda con este u otro programa puedes contratarme en
-            <a class="has-text-warning" href="https://parzibyte.me/#contacto"
-              >mi sitio web</a
-            >
+          <p class="subtitle">
+            Utiliza un enfoque basado en la personalización y la eficiencia.
+            Esta plataforma utiliza algoritmos y bases de datos para organizar y
+            clasificar las recetas de cocina, lo que permite a los usuarios realizar
+            búsquedas específicas y encontrar rápidamente las recetas que se ajusten a
+            sus preferencias
           </p>
-          <br />
-          <p>
-            No olvides pasarte por mi blog para enterarte de las novedades:
-            <a class="has-text-warning" href="https://parzibyte.me/blog"
-              >https://parzibyte.me/blog</a
-            >
+          <p class="subtitle">
+              © Wilver Emilio Xia Ixcot - 202108052, Hanz Maverick Godínez Ruíz - 202108064, Carlos Néstor Daniel Cano Velásquez - 202108031, Alexander Manuel Navar Vicente  - 202108068
+
           </p>
-          <strong
-            >Por cierto, este software no puede ser vendido. Puedes modificarlo
-            y usarlo para lo que prefieras, pero si quieres venderlo u obtener
-            ganancias de algún modo debes contar con mi autorización</strong
-          >
         </div>
+        <div class="contact-box-right">
+                <div class="google-map-wrap-layout1">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d123416.03330079452!2d-91.60340033228542!3d14.839224227256551!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x858e97fe3fc89d3f%3A0x3ba7ff011f0f000f!2sQuezaltenango!5e0!3m2!1ses-419!2sgt!4v1696477246411!5m2!1ses-419!2sgt" width="1255" height="360" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </div>
       </section>
       <h1></h1>
     </div>
   </div>
+
 </template>
+<style>
+
+</style>
+
+
